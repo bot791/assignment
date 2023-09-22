c5bb52bc4b27486b8163589b96061728cfa51ef4
kajal biswas <pikb2015@gmail.com>
2023-09-22 20:59:56 +0530

    adding styles

--- src/App.js
+++ src/App.js
@@ -2,85 +2,139 @@ import React from "react";
 
 export const App = () => {
   return (
-    <>
-      <div>
-        <h1>Be a Part of Something Great</h1>
-        <h4>
-          Quesque quam felis,volutpat sit amet scelerisque
-          <br />
-          eget,vestibulum blamditvelit.Nulla vestibulum
-        </h4>
-        <img src="./images/image 51.png" alt="" />
-      </div>
-      <div>
-        <div>
-          <div>
-            <h2>Large Beautiful Office</h2>
-            <h6>
-              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
-              felis,volutpat sit amet scelerisque
-            </h6>
-          </div>
-          <div>
-            <h2>Easy Location</h2>
-            <h6>
-              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
-              felis,volutpat sit amet scelerisque
-            </h6>
-          </div>
-          <div>
-            <h2>Performance Award</h2>
-            <h6>
-              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
-              felis,volutpat sit amet scelerisque
-            </h6>
-          </div>
-        </div>
-        <div>
-          <div>
-            <h2>Great Co-workers</h2>
-            <h6>
-              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
-              felis,volutpat sit amet scelerisque
-            </h6>
-          </div>
-          <div>
-            <h2>Educational Opportunities</h2>
-            <h6>
-              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
-              felis,volutpat sit amet scelerisque
-            </h6>
-          </div>
-          <div>
-            <h2>Easy Access to Leadership</h2>
-            <h6>
-              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
-              felis,volutpat sit amet scelerisque
-            </h6>
-          </div>
-        </div>
-        <div>
-          <h1>Perks and Benefits</h1>
-          <h4>
-            Quesque quam felis,volutpat sit amet scelerisque Quesque quam
-            felis,volutpat sit amet scelerisque
+    <div className=" bg-emerald-100 m-0 w-full">
+      <div className="">
+        <div className=" relative top-48">
+          <h1 className=" ml-10 text-3xl font-bold">
+            Be a Part of Something Great
+          </h1>
+          <h4 className=" ml-10 text-lg font-semibold">
+            Quesque quam felis,volutpat sit amet scelerisque
+            <br />
+            eget,vestibulum blamditvelit.Nulla vestibulum
           </h4>
         </div>
+        <img className=" w-[95%] ml-2" src="./images/image 51.png" alt="" />
       </div>
-      <div>
-        <h1>We are Looking For Talented People</h1>
-        <h4>
+      <div className=" w-[95%] mt-16 ml-10 flex flex-row">
+        <div className=" basis-1/4 flex flex-col">
+          <div className=" m-2 h-[250px] w-[250px] border ring-1 ring-emerald-600 rounded-md p-4 bg-white">
+            <img
+              className=" mt-4 h-[20px] w-[20px]"
+              src="./images/Vector-2.png"
+              alt=""
+            />
+            <h2 className=" mt-5 font-bold text-lg">Large Beautiful Office</h2>
+            <h6 className=" mt-3 font-semibold text-sm">
+              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+              felis,volutpat sit amet scelerisque
+            </h6>
+          </div>
+          <div className="m-2 h-[250px] w-[250px] border ring-1 ring-red-500 rounded-md p-4 bg-white">
+            <img
+              className=" mt-4 h-[20px] w-[20px]"
+              src="./images/Vector-4.png"
+              alt=""
+            />
+            <h2 className=" mt-5 font-bold text-lg">Easy Location</h2>
+            <h6 className=" mt-3 font-semibold text-sm">
+              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+              felis,volutpat sit amet scelerisque
+            </h6>
+          </div>
+          <div className="m-2 h-[250px] w-[250px] border ring-1 ring-pink-500 rounded-md p-4 bg-white">
+            <img
+              className=" mt-4 h-[20px] w-[20px]"
+              src="./images/Group 1000001907.png"
+              alt=""
+            />
+            <h2 className=" mt-5 font-bold text-lg">Performance Award</h2>
+            <h6 className=" mt-3 font-semibold text-sm">
+              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+              felis,volutpat sit amet scelerisque
+            </h6>
+          </div>
+        </div>
+        <div className=" basis-1/4 flex flex-col relative top-10 ">
+          <div className="m-2 h-[250px] w-[250px] border ring-1 ring-yellow-300 rounded-md p-4 bg-white">
+            <img
+              className=" mt-4 h-[20px] w-[20px]"
+              src="./images/Vector-3.png"
+              alt=""
+            />
+            <h2 className=" mt-5 font-bold text-lg">Great Co-workers</h2>
+            <h6 className=" mt-3 font-semibold text-sm">
+              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+              felis,volutpat sit amet scelerisque
+            </h6>
+          </div>
+          <div className="m-2 h-[250px] w-[250px] border ring-1 ring-sky-500 rounded-md p-4 bg-white">
+            <img
+              className=" mt-4 h-[20px] w-[20px]"
+              src="./images/Vector-5.png"
+              alt=""
+            />
+            <h2 className=" mt-5 font-bold text-lg">
+              Educational Opportunities
+            </h2>
+            <h6 className=" mt-3 font-semibold text-sm">
+              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+              felis,volutpat sit amet scelerisque
+            </h6>
+          </div>
+          <div className="m-2 h-[250px] w-[250px] border ring-1 ring-blue-700 rounded-md p-4 bg-white">
+            <img
+              className=" mt-4 h-[20px] w-[20px]"
+              src="./images/Vector-6.png"
+              alt=""
+            />
+            <h2 className=" mt-5 font-bold text-lg">
+              Easy Access to Leadership
+            </h2>
+            <h6 className=" mt-3 font-semibold text-sm">
+              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+              felis,volutpat sit amet scelerisque
+            </h6>
+          </div>
+        </div>
+        <div className=" basis-1/2 relative top-80">
+          <h1 className=" text-3xl font-bold">Perks and Benefits</h1>
+          <h4 className=" font-semibold text-md mt-5">
+            Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+            felis,volutpat sit amet scelerisque Quesque quam felis,volutpat sit
+            amet scelerisque Quesque quam felis,volutpat sit amet scelerisque
+          </h4>
+          <button className=" font-bold text-white mt-5 p-4 items-center bg-orange-400 hover:bg-blue-500 ring-2 ring-white rounded-md">
+            Learn more
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-row m-20">
+        <h1 className=" basis-1/3 text-3xl font-bold">
+          We are Looking For Talented People
+        </h1>
+        <h4 className=" basis-2/3 text-lg font-semibold">
           Quesque quam felis,volutpat sit amet scelerisque Quesque quam
-          felis,volutpat sit amet scelerisque
+          felis,volutpat sit amet scelerisque Quesque quam felis,volutpat sit
+          amet scelerisque Quesque quam felis,volutpat sit amet scelerisque
         </h4>
       </div>
       <div>
-        <div>
-          <h2>Current Openings</h2>
-          <h5>search jobs</h5>
+        <div className=" w-[88%] bg-white block m-auto p-2 rounded-sm">
+          <h2 className=" text-3xl font-bold text-center p-2">
+            Current Openings
+          </h2>
+          <input
+            className="m-auto block p-2 ring-2 ring-yellow-200 w-[70%]"
+            type="text"
+            placeholder="Search"
+          />
         </div>
-        <div>
-          <div>card</div>
+        <div className=" flex flex-row w-[85%] items-center m-2">
+          <div>
+            <h1>UI Developer</h1>
+            <h2>Lbkafe | Full-Time</h2>
+          </div>
           <div>card</div>
           <div>card</div>
           <div>card</div>
@@ -119,7 +173,7 @@ export const App = () => {
         <h1>lets get in touch</h1>
         <h2>form</h2>
       </div>
-    </>
+    </div>
   );
 };
 
