824b99627e662f235fb1d24cffd7e9583176cee9
kajal biswas <pikb2015@gmail.com>
2023-09-23 02:55:56 +0530

    more styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const App = () => {
   return (
-    <div className=" bg-emerald-100 m-0 w-full">
+    <div className="  m-0 w-full">
       <div className="">
         <div className=" relative top-48">
           <h1 className=" ml-10 text-3xl font-bold">
@@ -120,58 +120,181 @@ export const App = () => {
         </h4>
       </div>
       <div>
-        <div className=" w-[88%] bg-white block m-auto p-2 rounded-sm">
-          <h2 className=" text-3xl font-bold text-center p-2">
+        <div className="relative w-[88%] bg-white block m-auto p-2 rounded-sm">
+          <h1 className=" absolute top-2 right-2 text-xs font-light">
+            25 Jobs Available
+          </h1>
+          <h2 className=" text-3xl font-bold p-2 absolute top-0 left-1 ">
             Current Openings
           </h2>
-          <input
-            className="m-auto block p-2 ring-2 ring-yellow-200 w-[70%]"
-            type="text"
-            placeholder="Search"
-          />
-        </div>
-        <div className=" flex flex-row w-[85%] items-center m-2">
-          <div>
-            <h1>UI Developer</h1>
-            <h2>Lbkafe | Full-Time</h2>
+          <div className=" mt-14">
+            <input
+              className=" m-auto block  p-2 ring-2 ring-yellow-200 w-[70%]"
+              type="text"
+              placeholder="Search"
+            />
           </div>
-          <div>card</div>
-          <div>card</div>
-          <div>card</div>
+        </div>
+        <div className=" flex mt-16  m-2 space-x-5 justify-evenly">
+          <div className=" p-5 rounded-md bg-white">
+            <h1 className=" font-bold text-2xl">UI Developer</h1>
+            <h2 className=" font-semibold">Labkafe | Full-Time</h2>
+            <h5>
+              Responsibility:
+              <br />
+              *dfgd dgcg bfb ydcysg
+              <br />
+              *dfgd dgcg bfb ydcysg
+            </h5>
+            <button className=" mt-4 h-[40px] w-[100px] font-bold p-2 text-white bg-amber-500 hover:bg-teal-500 rounded-full text-center">
+              Apply
+            </button>
+          </div>
+          <div className=" p-5 rounded-md bg-white">
+            <h1 className=" font-bold text-2xl">UI Developer</h1>
+            <h2 className=" font-semibold">Labkafe | Full-Time</h2>
+            <h5>
+              Responsibility:
+              <br />
+              *dfgd dgcg bfb ydcysg
+              <br />
+              *dfgd dgcg bfb ydcysg
+            </h5>
+            <button className=" mt-4 h-[40px] w-[100px] font-bold p-2 text-white bg-amber-500 hover:bg-teal-500 rounded-full text-center">
+              Apply
+            </button>
+          </div>
+          <div className=" p-5 rounded-md bg-white">
+            <h1 className=" font-bold text-2xl">UI Developer</h1>
+            <h2 className=" font-semibold">Labkafe | Full-Time</h2>
+            <h5>
+              Responsibility:
+              <br />
+              *dfgd dgcg bfb ydcysg
+              <br />
+              *dfgd dgcg bfb ydcysg
+            </h5>
+            <button className=" mt-4 h-[40px] w-[100px] font-bold p-2 text-white bg-amber-500 hover:bg-teal-500 rounded-full text-center">
+              Apply
+            </button>
+          </div>
+          <div className=" p-5 rounded-md bg-white">
+            <h1 className=" font-bold text-2xl">UI Developer</h1>
+            <h2 className=" font-semibold">Labkafe | Full-Time</h2>
+            <h5>
+              Responsibility:
+              <br />
+              *dfgd dgcg bfb ydcysg
+              <br />
+              *dfgd dgcg bfb ydcysg
+            </h5>
+            <button className=" mt-4 h-[40px] w-[100px] font-bold p-2 text-white bg-amber-500 hover:bg-teal-500 rounded-full text-center">
+              Apply
+            </button>
+          </div>
         </div>
         <div>
-          <button>View all openings</button>
+          <button className=" mt-10  block m-auto p-2 rounded-full bg-amber-500 text-sm font-bold text-center text-white ">
+            View all openings
+          </button>
         </div>
       </div>
-      <div>
-        <div>
-          <h1>Fun at Workplace</h1>
+      <div className=" flex flex-row">
+        <div className=" basis-1/2 items-center m-10">
+          <h1 className=" text-center text-3xl font-bold">
+            Fun at
+            <br />
+            Workplace
+          </h1>
         </div>
-        <div>
-          <h5>
+        <div className=" basis-1/2 items-center m-10">
+          <h5 className=" text-left text-lg font-semibold">
             Quesque quam felis,volutpat sit amet scelerisque Quesque quam
-            felis,volutpat sit amet scelerisque
+            felis,volutpat sit amet scelerisque Quesque quam felis,volutpat sit
+            amet scelerisque Quesque quam felis,volutpat sit amet scelerisque
           </h5>
         </div>
       </div>
       <div>
-        <img src="./images/Rectangle 3150-1.png" alt="" />
-        <img src="./images/Rectangle 3150.png" alt="" />
-        <img src="./images/Rectangle 3151-1.png" alt="" />
-        <img src="./images/Rectangle 3151.png" alt="" />
-        <img src="./images/Rectangle 3152.png" alt="" />
-        <img src="./images/Rectangle 3153-1.png" alt="" />
-        <img src="./images/Rectangle 3153.png" alt="" />
-        <img src="./images/Rectangle 3154.png" alt="" />
+        <div className=" flex flex-row space-x-2 m-2">
+          <div>
+            <img src="./images/Rectangle 3150.png" alt="" />
+          </div>
+          <div>
+            <img className=" pb-3" src="./images/Rectangle 3152.png" alt="" />
+            <img src="./images/Rectangle 3153.png" alt="" />
+          </div>
+          <div>
+            <img src="./images/Rectangle 3151-1.png" alt="" />
+          </div>
+        </div>
+        <div className=" flex flex-row space-x-2 m-2">
+          <div>
+            <img src="./images/Rectangle 3150-1.png" alt="" />
+          </div>
+          <div>
+            <img src="./images/Rectangle 3151.png" alt="" />
+          </div>
+          <div>
+            <img className=" pb-3" src="./images/Rectangle 3154.png" alt="" />
+            <img src="./images/Rectangle 3153-1.png" alt="" />
+          </div>
+        </div>
       </div>
-      <div>
-        <div>contact us</div>
-        <div>call us</div>
-        <div>dont like forms</div>
-      </div>
-      <div>
-        <h1>lets get in touch</h1>
-        <h2>form</h2>
+      <div className=" flex flex-row mt-20 bg-white rounded-lg w-[75%] m-auto h-[455px] mb-10">
+        <div className=" grid grid-cols-2">
+          <div className=" col-span-1 bg-purple-300 m-2 h-[200px] w-[200px] rounded-lg border border-purple-700">
+            <img
+              className=" mt-4 h-[30px] w-[30px] ml-6"
+              src="./images/Group.png"
+              alt=""
+            />
+            <h1 className=" text-lg font-bold ml-6 mt-1">Contact Us</h1>
+            <h2 className=" text-sm font-semibold mt-2 ml-6">
+              We are on a mission to standardize lab education
+            </h2>
+            <button className=" text-white mt-2 ml-6 bg-purple-600 text-center p-2 rounded-full ring-1 ring-white hover:bg-purple-500">
+              Join us
+            </button>
+          </div>
+          <div className=" col-span-1 bg-lime-300 m-2 h-[200px] w-[200px] rounded-lg border border-lime-700">
+            <img
+              className=" mt-4 h-[30px] w-[30px] ml-6"
+              src="./images/Group-1.png"
+              alt=""
+            />
+            <h1 className="text-lg font-bold ml-6 mt-1">Call Us!</h1>
+            <h2 className=" text-sm font-semibold mt-2 ml-6">
+              Speak to our lab exparts we are happy to help
+            </h2>
+            <button className="text-white mt-2 ml-6 bg-lime-700 text-center p-2 rounded-full ring-1 ring-white hover:bg-lime-500">
+              Dive in
+            </button>
+          </div>
+
+          <div className=" col-span-2 bg-orange-300 m-2 h-[220px] w-[415px] rounded-lg border border-lime-700">
+            <img
+              className=" mt-4 h-[40px] w-[40px] ml-6"
+              src="./images/Vector-7.png"
+              alt=""
+            />
+            <h1 className=" text-lg font-bold ml-6 mt-2">Don't like Calls?</h1>
+            <h2 className=" text-sm font-semibold mt-4 ml-6">
+              We got you covered.Drop a mail or just a chat with us on
+              <br />
+              Whatsapp.We are available for you
+            </h2>
+            <button className="text-white mt-2 ml-6 bg-gray-700 text-center p-2 rounded-full ring-1 ring-white hover:bg-gray-500">
+              Chat
+            </button>
+            <button className="text-white mt-2 ml-6 bg-orange-700 text-center p-2 rounded-full ring-1 ring-white hover:bg-orange-500">
+              I prefer email
+            </button>
+          </div>
+        </div>
+        <div>
+          <div></div>
+        </div>
       </div>
     </div>
   );
