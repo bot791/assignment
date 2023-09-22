81b9e6042dbc8afe829e4ca4ca622d870f346cfc
kajal biswas <pikb2015@gmail.com>
2023-09-22 18:43:57 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>Be a Part of Something Great</h1>
+        <h4>
+          Quesque quam felis,volutpat sit amet scelerisque
+          <br />
+          eget,vestibulum blamditvelit.Nulla vestibulum
+        </h4>
+        <img src="./images/image 51.png" alt="" />
+      </div>
+      <div>
+        <div>
+          <div>
+            <h2>Large Beautiful Office</h2>
+            <h6>
+              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+              felis,volutpat sit amet scelerisque
+            </h6>
+          </div>
+          <div>
+            <h2>Easy Location</h2>
+            <h6>
+              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+              felis,volutpat sit amet scelerisque
+            </h6>
+          </div>
+          <div>
+            <h2>Performance Award</h2>
+            <h6>
+              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+              felis,volutpat sit amet scelerisque
+            </h6>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2>Great Co-workers</h2>
+            <h6>
+              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+              felis,volutpat sit amet scelerisque
+            </h6>
+          </div>
+          <div>
+            <h2>Educational Opportunities</h2>
+            <h6>
+              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+              felis,volutpat sit amet scelerisque
+            </h6>
+          </div>
+          <div>
+            <h2>Easy Access to Leadership</h2>
+            <h6>
+              Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+              felis,volutpat sit amet scelerisque
+            </h6>
+          </div>
+        </div>
+        <div>
+          <h1>Perks and Benefits</h1>
+          <h4>
+            Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+            felis,volutpat sit amet scelerisque
+          </h4>
+        </div>
+      </div>
+      <div>
+        <h1>We are Looking For Talented People</h1>
+        <h4>
+          Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+          felis,volutpat sit amet scelerisque
+        </h4>
+      </div>
+      <div>
+        <div>
+          <h2>Current Openings</h2>
+          <h5>search jobs</h5>
+        </div>
+        <div>
+          <div>card</div>
+          <div>card</div>
+          <div>card</div>
+          <div>card</div>
+        </div>
+        <div>
+          <button>View all openings</button>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h1>Fun at Workplace</h1>
+        </div>
+        <div>
+          <h5>
+            Quesque quam felis,volutpat sit amet scelerisque Quesque quam
+            felis,volutpat sit amet scelerisque
+          </h5>
+        </div>
+      </div>
+      <div>
+        <img src="./images/Rectangle 3150-1.png" alt="" />
+        <img src="./images/Rectangle 3150.png" alt="" />
+        <img src="./images/Rectangle 3151-1.png" alt="" />
+        <img src="./images/Rectangle 3151.png" alt="" />
+        <img src="./images/Rectangle 3152.png" alt="" />
+        <img src="./images/Rectangle 3153-1.png" alt="" />
+        <img src="./images/Rectangle 3153.png" alt="" />
+        <img src="./images/Rectangle 3154.png" alt="" />
+      </div>
+      <div>
+        <div>contact us</div>
+        <div>call us</div>
+        <div>dont like forms</div>
+      </div>
+      <div>
+        <h1>lets get in touch</h1>
+        <h2>form</h2>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
