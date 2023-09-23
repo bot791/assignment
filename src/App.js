65ce8d0f97a2d7d54dacd0347bfd1da8a0ae04aa
kajal biswas <pikb2015@gmail.com>
2023-09-23 14:40:27 +0530

    mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export const App = () => {
   return (
-    <div className="  m-0 w-full">
+    <div className="  sm:m-0 sm:w-full content-center w-[95%]">
       <div className="">
-        <div className=" relative top-48">
+        <div className=" sm:relative sm:top-48 block m-auto">
           <h1 className=" ml-10 text-3xl font-bold">
             Be a Part of Something Great
           </h1>
@@ -14,11 +14,15 @@ export const App = () => {
             eget,vestibulum blamditvelit.Nulla vestibulum
           </h4>
         </div>
-        <img className=" w-[95%] ml-2" src="./images/image 51.png" alt="" />
+        <img
+          className=" sm:w-[95%] sm:ml-2 ml-24 w-full relative top-0 right-2"
+          src="./images/image 51.png"
+          alt=""
+        />
       </div>
-      <div className=" w-[95%] mt-16 ml-10 flex flex-row">
-        <div className=" basis-1/4 flex flex-col">
-          <div className=" m-2 h-[250px] w-[250px] border ring-1 ring-emerald-600 rounded-md p-4 bg-white">
+      <div className=" sm:w-[95%] sm:mt-16 sm:ml-10 flex sm:flex-row flex-col items-center">
+        <div className=" sm:basis-1/4 sm:flex sm:flex-col m-4">
+          <div className=" sm:m-2 m-10 sm:h-[250px] sm:w-[250px] w-[95%] ml-14 border ring-1 ring-emerald-600 rounded-md p-4 bg-white">
             <img
               className=" mt-4 h-[20px] w-[20px]"
               src="./images/Vector-2.png"
@@ -30,7 +34,7 @@ export const App = () => {
               felis,volutpat sit amet scelerisque
             </h6>
           </div>
-          <div className="m-2 h-[250px] w-[250px] border ring-1 ring-red-500 rounded-md p-4 bg-white">
+          <div className="sm:m-2 m-10 sm:h-[250px] sm:w-[250px] w-[95%] ml-14 border ring-1 ring-red-500 rounded-md p-4 bg-white">
             <img
               className=" mt-4 h-[20px] w-[20px]"
               src="./images/Vector-4.png"
@@ -42,7 +46,7 @@ export const App = () => {
               felis,volutpat sit amet scelerisque
             </h6>
           </div>
-          <div className="m-2 h-[250px] w-[250px] border ring-1 ring-pink-500 rounded-md p-4 bg-white">
+          <div className="sm:m-2 sm:h-[250px] sm:w-[250px] w-[95%] ml-14 border ring-1 ring-pink-500 rounded-md p-4 bg-white">
             <img
               className=" mt-4 h-[20px] w-[20px]"
               src="./images/Group 1000001907.png"
@@ -55,8 +59,8 @@ export const App = () => {
             </h6>
           </div>
         </div>
-        <div className=" basis-1/4 flex flex-col relative top-10 ">
-          <div className="m-2 h-[250px] w-[250px] border ring-1 ring-yellow-300 rounded-md p-4 bg-white">
+        <div className=" sm:basis-1/4 flex flex-col relative top-10 ">
+          <div className="sm:m-2 sm:h-[250px] sm:w-[250px] w-[95%] ml-14 border ring-1 ring-yellow-300 rounded-md p-4 bg-white">
             <img
               className=" mt-4 h-[20px] w-[20px]"
               src="./images/Vector-3.png"
@@ -68,7 +72,7 @@ export const App = () => {
               felis,volutpat sit amet scelerisque
             </h6>
           </div>
-          <div className="m-2 h-[250px] w-[250px] border ring-1 ring-sky-500 rounded-md p-4 bg-white">
+          <div className="sm:m-2 m-6 sm:h-[250px] sm:w-[250px] w-[95%] ml-14 border ring-1 ring-sky-500 rounded-md p-4 bg-white">
             <img
               className=" mt-4 h-[20px] w-[20px]"
               src="./images/Vector-5.png"
@@ -82,7 +86,7 @@ export const App = () => {
               felis,volutpat sit amet scelerisque
             </h6>
           </div>
-          <div className="m-2 h-[250px] w-[250px] border ring-1 ring-blue-700 rounded-md p-4 bg-white">
+          <div className="sm:m-2 sm:h-[250px] sm:w-[250px] w-[95%] ml-14 border ring-1 ring-blue-700 rounded-md p-4 bg-white">
             <img
               className=" mt-4 h-[20px] w-[20px]"
               src="./images/Vector-6.png"
@@ -97,7 +101,7 @@ export const App = () => {
             </h6>
           </div>
         </div>
-        <div className=" basis-1/2 relative top-80">
+        <div className=" sm:basis-1/2 sm:top-80 mt-20 ml-24">
           <h1 className=" text-3xl font-bold">Perks and Benefits</h1>
           <h4 className=" font-semibold text-md mt-5">
             Quesque quam felis,volutpat sit amet scelerisque Quesque quam
@@ -110,17 +114,17 @@ export const App = () => {
         </div>
       </div>
       <div className="flex flex-row m-20">
-        <h1 className=" basis-1/3 text-3xl font-bold">
+        <h1 className=" sm:basis-1/3 basis-1/2 text-3xl font-bold">
           We are Looking For Talented People
         </h1>
-        <h4 className=" basis-2/3 text-lg font-semibold">
+        <h4 className=" sm:basis-2/3 ml-20 text-lg font-semibold">
           Quesque quam felis,volutpat sit amet scelerisque Quesque quam
           felis,volutpat sit amet scelerisque Quesque quam felis,volutpat sit
           amet scelerisque Quesque quam felis,volutpat sit amet scelerisque
         </h4>
       </div>
       <div>
-        <div className="relative w-[88%] bg-white block m-auto p-2 rounded-sm">
+        <div className="relative w-[88%] bg-white sm:block sm:m-auto ml-16 p-2 rounded-sm">
           <h1 className=" absolute top-2 right-2 text-xs font-light">
             25 Jobs Available
           </h1>
@@ -131,12 +135,12 @@ export const App = () => {
             <input
               className=" m-auto block  p-2 ring-2 ring-yellow-200 w-[70%]"
               type="text"
-              placeholder="Search"
+              placeholder="Search jobs"
             />
           </div>
         </div>
-        <div className=" flex mt-16  m-2 space-x-5 justify-evenly">
-          <div className=" p-5 rounded-md bg-white">
+        <div className=" sm:flex mt-16 m-2 sm:space-x-5 sm:justify-evenly ml-24">
+          <div className=" p-5 rounded-md bg-white m-5">
             <h1 className=" font-bold text-2xl">UI Developer</h1>
             <h2 className=" font-semibold">Labkafe | Full-Time</h2>
             <h5>
@@ -150,7 +154,7 @@ export const App = () => {
               Apply
             </button>
           </div>
-          <div className=" p-5 rounded-md bg-white">
+          <div className=" p-5 rounded-md bg-white m-5">
             <h1 className=" font-bold text-2xl">UI Developer</h1>
             <h2 className=" font-semibold">Labkafe | Full-Time</h2>
             <h5>
@@ -164,7 +168,7 @@ export const App = () => {
               Apply
             </button>
           </div>
-          <div className=" p-5 rounded-md bg-white">
+          <div className=" p-5 rounded-md bg-white m-5">
             <h1 className=" font-bold text-2xl">UI Developer</h1>
             <h2 className=" font-semibold">Labkafe | Full-Time</h2>
             <h5>
@@ -178,7 +182,7 @@ export const App = () => {
               Apply
             </button>
           </div>
-          <div className=" p-5 rounded-md bg-white">
+          <div className=" p-5 rounded-md bg-white m-5">
             <h1 className=" font-bold text-2xl">UI Developer</h1>
             <h2 className=" font-semibold">Labkafe | Full-Time</h2>
             <h5>
@@ -216,34 +220,66 @@ export const App = () => {
         </div>
       </div>
       <div>
-        <div className=" flex flex-row space-x-2 m-2">
+        <div className=" sm:flex sm:flex-row sm:space-x-2 sm:m-2">
           <div>
-            <img src="./images/Rectangle 3150.png" alt="" />
+            <img
+              className="sm:m-0 m-5 ml-10"
+              src="./images/Rectangle 3150.png"
+              alt=""
+            />
           </div>
           <div>
-            <img className=" pb-3" src="./images/Rectangle 3152.png" alt="" />
-            <img src="./images/Rectangle 3153.png" alt="" />
+            <img
+              className=" sm:pb-3 sm:m-0 m-5 ml-16"
+              src="./images/Rectangle 3152.png"
+              alt=""
+            />
+            <img
+              className="sm:m-0 m-5 ml-16"
+              src="./images/Rectangle 3153.png"
+              alt=""
+            />
           </div>
           <div>
-            <img src="./images/Rectangle 3151-1.png" alt="" />
+            <img
+              className="sm:m-0 m-5 ml-10"
+              src="./images/Rectangle 3151-1.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className=" flex flex-row space-x-2 m-2">
+        <div className=" sm:flex sm:flex-row sm:space-x-2 sm:m-2">
           <div>
-            <img src="./images/Rectangle 3150-1.png" alt="" />
+            <img
+              className="sm:m-0 m-5 ml-10"
+              src="./images/Rectangle 3150-1.png"
+              alt=""
+            />
           </div>
           <div>
-            <img src="./images/Rectangle 3151.png" alt="" />
+            <img
+              className="sm:m-0 m-5 ml-10"
+              src="./images/Rectangle 3151.png"
+              alt=""
+            />
           </div>
           <div>
-            <img className=" pb-3" src="./images/Rectangle 3154.png" alt="" />
-            <img src="./images/Rectangle 3153-1.png" alt="" />
+            <img
+              className=" sm:pb-3 sm:m-0 m-5 ml-20"
+              src="./images/Rectangle 3154.png"
+              alt=""
+            />
+            <img
+              className="sm:m-0 m-5 ml-20"
+              src="./images/Rectangle 3153-1.png"
+              alt=""
+            />
           </div>
         </div>
       </div>
-      <div className=" flex flex-row mt-20 bg-white rounded-lg w-[75%] m-auto h-[455px] mb-10">
-        <div className=" grid grid-cols-2">
-          <div className=" col-span-1 bg-purple-300 m-2 h-[200px] w-[200px] rounded-lg border border-purple-700">
+      <div className=" sm:flex sm:flex-row sm:space-x-4 sm:mt-20 sm:bg-white rounded-lg w-[80%] sm:m-auto h-[455px] sm:mb-10">
+        <div className=" sm:grid sm:grid-cols-2">
+          <div className="col-span-1 bg-purple-300 m-2 h-[200px] sm:w-[200px] w-[415px] rounded-lg border border-purple-700">
             <img
               className=" mt-4 h-[30px] w-[30px] ml-6"
               src="./images/Group.png"
@@ -257,7 +293,7 @@ export const App = () => {
               Join us
             </button>
           </div>
-          <div className=" col-span-1 bg-lime-300 m-2 h-[200px] w-[200px] rounded-lg border border-lime-700">
+          <div className=" col-span-1 bg-lime-300 m-2 h-[200px] sm:w-[200px] w-[415px] rounded-lg border border-lime-700">
             <img
               className=" mt-4 h-[30px] w-[30px] ml-6"
               src="./images/Group-1.png"
@@ -293,7 +329,70 @@ export const App = () => {
           </div>
         </div>
         <div>
-          <div></div>
+          <div className=" bg-white rounded-lg sm:p-0 p-10 w-full  sm:ml-0 ml-14 sm:mt-0 mt-10  mb-10 ">
+            <h1 className=" text-3xl font-bold mt-2 mb-4">
+              Let's get in touch
+            </h1>
+            <form>
+              <label className="mt-4" for="fname">
+                First name:
+              </label>
+              <input
+                className=" border-b border-gray-400 mt-4"
+                type="text"
+                id="fname"
+                name="fname"
+              />
+              <label className=" mt-4" for="lname">
+                Last name:
+              </label>
+              <input
+                className=" border-b border-gray-400 mt-4"
+                type="text"
+                id="lname"
+                name="lname"
+              />
+              <br />
+              <label className=" mt-4" for="fname">
+                Email:
+              </label>
+              <input
+                className=" border-b border-gray-400 mt-4"
+                type="text"
+                id="fname"
+                name="fname"
+              />
+              <label className=" mt-4" for="lname">
+                Mobile number:
+              </label>
+              <input
+                className=" border-b border-gray-400 mt-4"
+                type="text"
+                id="lname"
+                name="lname"
+              />
+              <br />
+              <p className=" font-bold text-lg mt-5">Select subject:</p>
+               
+              <input type="radio" id="html" name="fav_language" /> 
+              <label for="html">General Inquiry</label>
+                <input type="radio" id="css" name="fav_language" /> 
+              <label for="css">Complain</label>
+              <input type="radio" id="javascript" name="fav_language" /> 
+              <label for="javascript">Specific</label>
+              <input type="radio" id="javascript" name="fav_language" /> 
+              <label for="javascript">Other</label>
+              <br />
+              <p className=" mt-5">
+                <label for="message">Message:</label>
+              </p>
+              <textarea className=" mt-5 border-b border-gray-400 sm:h-[100px] sm:w-[415px] w-[200px]"></textarea>
+              <br />
+              <button className=" p-2 text-center rounded-lg bg-orange-500 hover:bg-orange-200 ring-1 ring-gray-500 text-white font-bold m-2">
+                Send Message
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
